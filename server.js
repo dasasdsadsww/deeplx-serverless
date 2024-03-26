aaf1d047506590a511bf4516bc8bd4d9ddbd95fc
testman <testboy@test.com1>
2024-03-26 17:40:34 +0900

    test

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post('/translate', async (req, res) => {
   }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, '0.0.0.0/', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
