39d1f80a134a65c6e052af766cff54012ce60d21
testman <testboy@test.com1>
2024-03-26 16:51:03 +0900

    fix error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { translate } = require('./translate');
 const cors = require('express')
+const cors = require('cors')
 
 const app = express();
 const PORT = 9000;
